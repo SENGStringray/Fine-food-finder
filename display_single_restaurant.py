1f06963e1c967aea98cd42d542e9dba55f293f74
Chris Bui <christine.tvbui@gmail.com>
2017-04-28 17:27:43 +1000

    add display-restaurant func

---
+++ display_single_restaurant.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt4 import QtCore, QtGui
+#Name of restaurant supplied as a STRING
+
+	def display_restaurant(name, restaurant_names):
+		#find match in array of restaurant names
+		if name in restaurant_names:
+			#display page
+		else:
+			break;
+			#display error dialog
+			print "Error: invalid restaurant name"
