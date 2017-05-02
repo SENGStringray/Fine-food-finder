11f454797d0f5fc8d8b91f8efb278f724da1fdc7
justindaerolee <daero1995lee@gmail.com>
2017-05-02 18:34:58 +1000

    Update search.py

--- InputSearch/search.py
+++ InputSearch/search.py
@@ -2,6 +2,14 @@
 
 import sys, re, os
 
+class Restuarant:
+        def __init__(self, name, price, rating):
+                self.name = name
+                self.price = price
+                self.rating = rating
+        def __repr__(self):
+                return repr((self.name, self.price, self.rating))
+
 searchString = sys.stdin.readline()
 searchString.rstrip()
 path = "/Users/justindaerolee/School/seng2011/project" # write the path
@@ -17,14 +25,7 @@ for fileN in glob.glob(os.path.join(path, '*.txt')): # doesn't seem to work
 for fileN in ArrOfRestuarants:
 	print(fileN)
 
-class Restuarant:
-        def __init__(self, name, price, rating):
-                self.name = name
-                self.price = price
-                self.rating = rating
-        def __repr__(self):
-                return repr((self.name, self.price, self.rating))
-        
-
+# sorted by name(default)
+#comparator = "name"
 sorted(arrOfRestuarants, key=lambda Restuarant: Restuarant.name)
 
