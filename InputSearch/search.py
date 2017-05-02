5e14e716eb3f94e17a65f14b80eb1e75135e3bcf
justindaerolee <daero1995lee@gmail.com>
2017-05-02 17:40:17 +1000

    search.py
    searches for restuarants

---
+++ InputSearch/search.py
@@ -0,0 +1,30 @@
+#!/usr/bin/py
+
+import sys, re, os
+
+searchString = sys.stdin.readline()
+searchString.rstrip()
+path = "/Users/justindaerolee/School/seng2011/project" # write the path
+arrOfRestuarants = []
+for fileN in glob.glob(os.path.join(path, '*.txt')): 
+	f  = open(fileN, “r”)
+	content = f.read()
+	match = re.search(searchString, content)
+	if (match):
+		arrOfRestuarants.apphend(fileN)
+	f.close
+
+for fileN in ArrOfRestuarants:
+	print(fileN)
+
+class Restuarant:
+        def __init__(self, name, price, rating):
+                self.name = name
+                self.price = price
+                self.rating = rating
+        def __repr__(self):
+                return repr((self.name, self.price, self.rating))
+        
+
+sorted(arrOfRestuarants, key=lambda Restuarant: Restuarant.name)
+
